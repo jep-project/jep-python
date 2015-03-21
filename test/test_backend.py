3c8a4510f58660b88f8bc267fcaf1c1b9d712b28
Mike Pagel <mike@mpagel.de>
2015-03-21 20:07:45 +0100

    Tested one bind/listen/accept/close cycle.

--- test/test_backend.py
+++ test/test_backend.py
@@ -14,9 +14,9 @@ def test_initial_state():
 def test_find_server_port(mock_socket_mod):
     # deny binding to any port:
     mock_socket_mod.socket().bind = mock.MagicMock(side_effect=OSError)
+    backend = Backend()
 
     with pytest.raises(NoPortFoundError):
-        backend = Backend()
         backend.start()
 
     assert PORT_RANGE[0] < PORT_RANGE[1], 'At lest one port should be testable.'
@@ -24,3 +24,34 @@ def test_find_server_port(mock_socket_mod):
     # check if all ports have been tested:
     for port in range(PORT_RANGE[0], PORT_RANGE[1]):
         assert mock.call(('localhost', port)) in mock_socket_mod.socket().bind.call_args_list
+
+    assert backend.state is State.Stopped
+
+
+def set_backend_state(backend, state, return_value=None):
+    """Utility to set backend state from outside as mock side-effect."""
+
+    def _(*args):
+        backend.state = state
+        return return_value
+
+    return _
+
+
+@mock.patch('jep.backend.socket')
+@mock.patch('jep.backend.select')
+def test_bind_and_listen_and_accept_and_disconnect(mock_select_mod, mock_socket_mod, capsys):
+    backend = Backend()
+    server_socket = mock_socket_mod.socket()
+
+    # mock a connecting frontend:
+    mock_select_mod.select = mock.MagicMock(return_value=([server_socket], [], []))
+    client_socket = mock.MagicMock()
+    server_socket.accept = mock.MagicMock(side_effect=set_backend_state(backend, State.ShutdownPending, [client_socket]))
+    backend.start()
+    assert backend.state is State.Stopped
+    server_socket.close.assert_called_once()
+    client_socket.close.assert_called_once()
+
+    out, *_ = capsys.readouterr()
+    assert 'JEP service, listening on port 9001' in out
