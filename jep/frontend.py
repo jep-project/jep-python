30ac2934a4973aa1c81a4a401f2c0f3adebe1010
Mike Pagel <mike@mpagel.de>
2015-03-23 18:49:50 +0100

    Added callback missing message CompletionInvocation.

--- jep/frontend.py
+++ jep/frontend.py
@@ -22,6 +22,9 @@ class BackendListener:
     def on_out_of_sync(self, out_of_sync, context):
         return NotImplemented
 
+    def on_content_sync(self, content_sync, context):
+        return NotImplemented
+
     def on_problem_update(self, problem_update, context):
         return NotImplemented
 
