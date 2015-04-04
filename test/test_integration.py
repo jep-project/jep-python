31104b0a099ffe82d6528a1216a3d6c19c982f58
Mike Pagel <mike@mpagel.de>
2015-04-04 17:45:19 +0200

    Made integration of frontend/backend an automated test.

--- test/test_integration.py
+++ test/test_integration.py
@@ -3,7 +3,6 @@ import logging.config
 import sys
 import datetime
 from jep.frontend import Frontend, BackendListener, State
-from jep.schema import Shutdown
 
 logging.config.dictConfig({
     'version': 1,
@@ -38,18 +37,18 @@ _logger = logging.getLogger('jep.frontend.sample')
 
 class MyListener(BackendListener):
     def on_backend_alive(self, context):
-        #context.send_message(Shutdown())
         context.disconnect()
 
 
-frontend = Frontend([MyListener()])
-connection = frontend.get_connection('localfile.mydsl')
+def test_frontend_starts_stops_backend():
+    frontend = Frontend([MyListener()])
+    connection = frontend.get_connection('../scripts/localfile.mydsl')
 
-started = datetime.datetime.now()
-cycles = 0
-while connection.state is not State.Disconnected:
-    connection.run(datetime.timedelta(seconds=0.1))
-    cycles += 1
-ended = datetime.datetime.now()
+    started = datetime.datetime.now()
+    cycles = 0
+    while connection.state is not State.Disconnected:
+        connection.run(datetime.timedelta(seconds=0.1))
+        cycles += 1
+    ended = datetime.datetime.now()
 
-_logger.info('Frontend executed %d cycles in %.2f seconds.' % (cycles, (ended - started).total_seconds()))
+    _logger.info('Frontend executed %d cycles in %.2f seconds.' % (cycles, (ended - started).total_seconds()))
